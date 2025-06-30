471a231d591e954b475ac1e7cc2f419266da4d87
Смирнов Александр Михайлович <smirnov@pof-flexo.spb.ru>
2025-06-30 10:03:53 +0300

    Start creating the documentation

---
+++ pycritic/doc/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../src"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PyCritic'
+copyright = '2025, Alexander Smirnov'
+author = 'Alexander Smirnov'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Disable prepending module names
+add_module_names = False
+
+# Sort members by type
+autodoc_member_order = 'groupwise'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ pycritic/doc/src.rst
@@ -0,0 +1,29 @@
+src package
+===========
+
+Submodules
+----------
+
+src.checker module
+------------------
+
+.. automodule:: src.checker
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+src.criterion module
+--------------------
+
+.. automodule:: src.criterion
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: src
+   :members:
+   :show-inheritance:
+   :undoc-members:
