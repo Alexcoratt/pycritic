c829c3082b5a696ad3359c709c6e58fc08ee4dac
Смирнов Александр Михайлович <smirnov@pof-flexo.spb.ru>
2025-06-30 11:44:08 +0300

    Document the pycritic_json.src.suite_builder module

--- pycritic_json/src/suite_builder.py
+++ pycritic_json/src/suite_builder.py
@@ -14,12 +14,25 @@ from .criterion_builder import \
 
 
 class DefaultSuiteBuilder(CriterionBuilder[Estimation]):
+	"""Default suite builder
+	
+	The suite builder which is used by default
+	"""
+
 	CRITERION_BUILDER = DefaultCriterionBuilder()
+	"""Criterion builder"""
+
 	CRITERIA_KEY = "crit"
+	"""Key of the criteria parameter in a config"""
+
 	ESTIMAND_SCHEMA_KEY = "schema"
+	"""Key of the schema parameter in a config"""
 
 	SCHEMA_ENV_KEY = "PYCRITIC_SUITE_SCHEMA"
+	"""Key of the environmental variable storing path to a schema for a suite"""
+
 	DEFAULT_SCHEMA_FILENAME = "../schemas/suite.schema.json"
+	"""Default relative path to a schema for a suite"""
 
 
 	def __init__(self) -> None:
@@ -27,6 +40,7 @@ class DefaultSuiteBuilder(CriterionBuilder[Estimation]):
 
 
 	def loadSchema(self) -> None:
+		"""Load a JSON schema"""
 		filename = DefaultSuiteBuilder.getSchemaFilename()
 		with open(filename) as file:
 			self.__schema = json.load(file)
@@ -34,6 +48,11 @@ class DefaultSuiteBuilder(CriterionBuilder[Estimation]):
 
 	@staticmethod
 	def getSchemaFilename() -> str:
+		"""Get a filepath to a schema
+		
+		:return: Filepath to a schema
+		:rtype: str
+		"""
 		try:
 			return os.environ[DefaultSuiteBuilder.SCHEMA_ENV_KEY]
 		except KeyError:
@@ -45,6 +64,13 @@ class DefaultSuiteBuilder(CriterionBuilder[Estimation]):
 
 
 	def __call__(self, raw: t.Any) -> pycritic.Criterion[Estimation]:
+		"""
+		
+		Validates the parameter value using the schema and decides which one criterion to create
+
+		:param raw: A raw data to convert
+		:type raw: typing.Any
+		"""
 		jsonschema.validate(raw, self.__schema)
 
 		if not isinstance(raw, t.Mapping):
